de112b645e3a7687e7adf7ca5bd079f233943db3
cesar <cesar@DESKTOP-93H17NP>
2024-05-12 19:59:07 -0300

    Agregar archivos de proyecto.

---
+++ BlazorPeliculas/Client/Helpers/IJSRuntimeExtensionMethods.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.JSInterop;
+
+namespace BlazorPeliculas.Client.Helpers
+{
+    public static class IJSRuntimeExtensionMethods
+    {
+        public static async ValueTask<bool> Confirm(this IJSRuntime js, string mensaje)
+        {
+            await js.InvokeVoidAsync("console.log", "Prueba de console log");
+            return await js.InvokeAsync<bool>("confirm", mensaje);
+        }
+    }
+}
