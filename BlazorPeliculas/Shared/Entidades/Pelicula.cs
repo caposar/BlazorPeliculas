773e4da664c51c3a556481c8c83e6142a3a3d6cb
Cesar Paredes <chpvhr@gmail.com>
2024-05-18 21:52:10 -0300

    Se completo unidad 5. Formularios

--- BlazorPeliculas/Shared/Entidades/Pelicula.cs
+++ BlazorPeliculas/Shared/Entidades/Pelicula.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,9 +9,15 @@ namespace BlazorPeliculas.Shared.Entidades
 {
     public class Pelicula
     {
+        public int Id { get; set; }
+        [Required]
         public string Titulo { get; set; } = null!;
-        public DateTime FechaLanzamiento { get; set; }
-        public string Poster { get; set; } = null!;
+        public string? Resumen { get; set; }
+        public bool EnCartelera { get; set; }
+        public string? Trailer { get; set; }
+        public DateTime? Lanzamiento { get; set; }
+        public string? Poster { get; set; }
+        public List<GeneroPelicula> GenerosPelicula { get; set; } = new List<GeneroPelicula>();
         public string? TituloCortado
         {
             get
